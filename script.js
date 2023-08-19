4b6c02cbfb3b182a632c23d22c2f0b7afe3088c7
amruthakalyan <116357692+amruthakalyan@users.noreply.github.com>
2023-08-19 17:21:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,25 +11,41 @@ let QuizData = [
       correctAnswer: "Mercury"
     },
     {
-      question: "3.What is 7 + 3?",
+      question: "3.Chola’s capital city was ___",
+      options: [" Kanchipuram", " Madurai", "Tiruchy", "Tanjore"],
+      correctAnswer: "Tanjore"
+    },
+    {
+      question: "4.What is 7 + 3?",
       options: ["10", "12", "14", "8"],
       correctAnswer: "10"
     },
     {
-        question: "4.Who is the prime minister of india",
+      question: "5.The founder of the Sikh religion was ___",
+      options: ["Guru Nanak", " Tukaram", "Ramanada", " Kabir"],
+      correctAnswer: "Guru Nanak"
+    },
+
+    {
+        question: "6.Who is the prime minister of india",
         options: ["Narendra Modi", "Rahul gandhi", "KCR", "mamatha benergi"],
         correctAnswer: "Narendra Modi"
       },
       {
-        question: "5.What is our natioal bird?",
+        question: "7.What is our natioal bird?",
         options: ["crow", "sparrow", "peacock", "pigeon"],
         correctAnswer: "peacock"
       },
       {
-        question: "6.What does js stands for?",
+        question: "8.What does js stands for?",
         options: ["javastand", "javascript", "javascope", "javaschool"],
         correctAnswer: "javascript"
-      }
+      },
+      {
+        question: "9.Duration of basketball match both before & after the break?",
+        options: ["25 minutes", " 35 minutes", "45 minutes", "60 minutes"],
+        correctAnswer: "25 minutes"
+      },
   ];
   
 let questions = document.getElementById("question");
@@ -72,7 +88,7 @@ else{
   score.innerHTML=score.innerHTML;
   alert("your answer is wrong");
 }
-showscore.innerHTML = `Your final score is ${scores}/6`
+showscore.innerHTML = `Your final score is ${scores}/9`
 }
 let showscore=document.getElementById('showscore');
 
